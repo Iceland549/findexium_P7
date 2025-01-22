bf3ca09760d3d97b5356c241309adc4dc01a99a5
chaud <chaudenisbreiz@yahoo.fr>
2025-01-22 08:45:50 +0100

    Add buil.Services for Identity to Program.cs

--- P7CreateRestApi/Program.cs
+++ P7CreateRestApi/Program.cs
@@ -2,6 +2,7 @@ using P7CreateRestApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
 using P7CreateRestApi.Repositories;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
@@ -16,6 +17,14 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<LocalDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+
 builder.Services.AddScoped<BidListRepository>();
 builder.Services.AddScoped<CurvePointRepository>();
 builder.Services.AddScoped<RatingRepository>();
@@ -24,6 +33,9 @@ builder.Services.AddScoped<TradeRepository>();
 builder.Services.AddScoped<UserRepository>();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
