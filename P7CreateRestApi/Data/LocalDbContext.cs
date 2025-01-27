66719033bf0ddba829ac174f7c2a5306f4c82f3f
chaud <chaudenisbreiz@yahoo.fr>
2025-01-27 09:28:39 +0100

    Add "null" to all DbSet

--- P7CreateRestApi/Data/LocalDbContext.cs
+++ P7CreateRestApi/Data/LocalDbContext.cs
@@ -16,12 +16,12 @@ namespace P7CreateRestApi.Data
             base.OnModelCreating(builder);
         }
 
-        public DbSet<BidList> BidLists { get; set; }
-        public DbSet<Trade> Trades { get; set; }
-        public DbSet<CurvePoint> CurvePoints { get; set; }
-        public DbSet<Rating> Ratings { get; set; }
-        public DbSet<RuleName> RuleNames { get; set; }
-        public DbSet<User> Users { get; set; }
+        public DbSet<BidList> BidLists { get; set; } = null!;
+        public DbSet<Trade> Trades { get; set; } = null!;
+        public DbSet<CurvePoint> CurvePoints { get; set; } = null!;
+        public DbSet<Rating> Ratings { get; set; } = null!;
+        public DbSet<RuleName> RuleNames { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
 
     }
 }
