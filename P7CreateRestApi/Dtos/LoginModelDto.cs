3c24e3b1c2625757466e9e21abdc76bbfeefa5f8
chaud <chaudenisbreiz@yahoo.fr>
2025-01-30 09:29:37 +0100

    Modify RegisterModelDto and LoginModelDto

---
+++ P7CreateRestApi/Dtos/LoginModelDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P7CreateRestApi.Dtos
+{
+    public class LoginModelDto
+    {
+        //[Required(ErrorMessage ="L'email est obligatoire.")]
+        //[EmailAddress(ErrorMessage = "L'email n'est pas valide")]
+        //public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Le champ est obligatoire.")]
+        public string? UserName { get; set; }
+
+
+        [Required(ErrorMessage = "Le champ est obligatoire.")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
---
+++ P7CreateRestApi/Dtos/RegisterModelDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P7CreateRestApi.Dtos
+{
+    public class RegisterModelDto
+    {
+
+        [Required(ErrorMessage = "Le nom d'utilisateur est obligatoire.")]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage = "L'email est obligatoire.")]
+        [EmailAddress(ErrorMessage = "L'email n'est pas valide.")]
+        [StringLength(255, ErrorMessage = "L'email ne peut pas dépasser 255 caractères.")]
+
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Le mot de passe est obligatoire.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "Le mot de passe doit comporter au moins {2} caractères.", MinimumLength = 8)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", 
+        ErrorMessage = "Le mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Ce champ est obligatoire.")]
+
+        public string? FullName { get; set; }
+    }
+}
