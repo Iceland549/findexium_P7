40fd2839142cea88942f87250331bab228a33895
chaud <chaudenisbreiz@yahoo.fr>
2025-01-30 13:33:22 +0100

    Create LoggingMiddleware.cs

---
+++ P7CreateRestApi/LoggingMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace P7CreateRestApi
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public LoggingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<LoggingMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _logger.LogInformation("Request {Method} {Path} started", context.Request.Method, context.Request.Path);
+
+            await _next(context);
+
+            _logger.LogInformation("Request {Method} {Path} completed with status code {StatusCode}",
+                context.Request.Method, context.Request.Path, context.Response.StatusCode);
+        }
+
+    }
+
+    public static class LoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLoggingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LoggingMiddleware>();
+        }
+    }
+
+}
